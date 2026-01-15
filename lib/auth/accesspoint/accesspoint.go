e032934608ff76f1a0b1f8df50ee1fd137a2d6c8
Dustin Specker <dustin.specker@goteleport.com>
2026-01-14 21:20:30 -0600

    feat(lib/auth): setup cache with workload clusters

--- lib/auth/accesspoint/accesspoint.go
+++ lib/auth/accesspoint/accesspoint.go
@@ -115,6 +115,7 @@ type Config struct {
 	RecordingEncryption     services.RecordingEncryption
 	Plugin                  services.Plugins
 	AppAuthConfig           services.AppAuthConfigReader
+	WorkloadClusterService  services.WorkloadClusterService
 }
 
 func (c *Config) CheckAndSetDefaults() error {
@@ -202,6 +203,7 @@ func NewCache(cfg Config) (*cache.Cache, error) {
 		RecordingEncryption:     cfg.RecordingEncryption,
 		Plugin:                  cfg.Plugin,
 		AppAuthConfig:           cfg.AppAuthConfig,
+		WorkloadClusterService:  cfg.WorkloadClusterService,
 	}
 
 	return cache.New(cfg.Setup(cacheCfg))
